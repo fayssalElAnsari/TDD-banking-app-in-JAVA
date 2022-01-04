9bf01cb28ca8e1e920cfe5b640d02ccdd69d4dce
fayssalElAnsari <fayssal.el.ansari@gmail.com>
2022-01-05 00:13:49 +0100

    new bank test V

--- src/main/Bank.java
+++ src/main/Bank.java
@@ -1,20 +1,27 @@
 package main;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import account.Account;
 import account.SavingsAccount;
 
 public class Bank {
+	
+	protected List<Account> accounts;
+	protected List<SavingsAccount> savingsAccounts;
+	
+	public Bank() {
+		this.accounts = new ArrayList<Account>();
+		this.savingsAccounts = new ArrayList<SavingsAccount>();
+	}
 
 	public ArrayList<Account> getAccounts() {
-		// TODO Auto-generated method stub
-		return null;
+		return (ArrayList<Account>) this.accounts;
 	}
 
 	public ArrayList<SavingsAccount> getSavingsAccounts() {
-		// TODO Auto-generated method stub
-		return null;
+		return (ArrayList<SavingsAccount>) this.savingsAccounts;
 	}
 
 }
